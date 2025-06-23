f22d2488b475616ac91f7aa06ea4f1242a31cc71
Meha <mixiadami@gmail.com>
2025-06-23 11:37:13 +0200

    CRATE ADD SCORE/LOSE GAME/TO HIGHT-TO LOW

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 'use strict';
-document.querySelector('.message');
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 21;
+// document.querySelector('.guess').value = 23;
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '👍Currect Number';
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🚫To Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lose';
+    }
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🚫To Hight!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lose';
+    }
+  }
+});
