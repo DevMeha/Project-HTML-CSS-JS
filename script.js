475b30834da419fa11f2ac14a3a832799047ac26
Meha <mixiadami@gmail.com>
2025-06-24 00:15:31 +0200

    ADD BACKGROUND COLOR WIN

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🚫No number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '👍Currect Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '🚫To Low!';
