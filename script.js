0899c05eb0ffebd2d069e3e3a675e919264c6a93
Meha <mixiadami@gmail.com>
2025-06-24 00:56:01 +0200

    ADD AGAIN STYLE BUTTTON

--- script.js
+++ script.js
@@ -5,9 +5,8 @@
 // document.querySelector('.score').textContent = 21;
 // document.querySelector('.guess').value = 23;
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -18,6 +17,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '👍Currect Number';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '🚫To Low!';
@@ -25,6 +25,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'You Lose';
+      score = 0;
+      document.querySelector('.score').textContent = score;
     }
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -33,6 +35,19 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'You Lose';
+      score = 0;
+      document.querySelector('.score').textContent = '0';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
 });
