2bfa7887ed1b08ef30f24a031d3cd37303148fe3
Meha <mixiadami@gmail.com>
2025-06-24 19:36:28 +0200

    ADD HIGHSCORE

--- script.js
+++ script.js
@@ -18,6 +18,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+    let highscore = 0;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '🚫To Low!';
