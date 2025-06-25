4786feecfd08fbf2abe8aaba76a5723f97639470
Meha <mixiadami@gmail.com>
2025-06-25 19:54:31 +0200

    ADD CLOSE WINDOW CLOSING WINDOW USING ESC

--- 2-page/script-next-page.js
+++ 2-page/script-next-page.js
@@ -22,3 +22,11 @@ const closeModal = function () {
 };
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
+});
