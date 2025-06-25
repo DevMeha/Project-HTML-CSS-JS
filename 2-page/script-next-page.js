02e10dd9a1edd480f680780e0a58b1efba964ca5
Meha <mixiadami@gmail.com>
2025-06-25 14:32:10 +0200

    ADD OPEN/CLOSE BTN AND CLOSE CLICK BACKGROUND

--- 2-page/script-next-page.js
+++ 2-page/script-next-page.js
@@ -3,6 +3,22 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
+const OpenModal = function () {
+  console.log("Button Clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// OPEN FUNCTION AND ADD BTN OPEN
+
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener("click", OpenModal);
+
+// CLOSE FUNCTION AND ADD TO BTN CLOSE
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
